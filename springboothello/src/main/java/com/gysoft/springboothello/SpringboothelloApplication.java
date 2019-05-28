6e578d72a5e873addf1e80233e139e1ab5f96c7c
wws11 <17637900215@163.com>
2019-05-28 11:33:06 +0800

    初始化springboot

---
+++ springboothello/src/main/java/com/gysoft/springboothello/SpringboothelloApplication.java
@@ -0,0 +1,13 @@
+package com.gysoft.springboothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothelloApplication.class, args);
+    }
+
+}
